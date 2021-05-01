6e9820260a7307631d43a203a85bd70d097f1a85
Abel Souza <abelsouzacosta@gmail.com>
2021-05-01 00:26:12 -0300

    feat(calculator): rendering well organized elements inside the calculator component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import './Button.css';
 
 const Button = (props) => {
+  let classes = 'button ';
+  classes += props.operation ? 'operation': '';
+  classes += props.double ? 'double': '';
+  classes += props.triple ? 'triple' : '';
+
   return (
-    <button className="button">{ props.label }</button>
+    <button 
+    onClick={ e => props.click && props.click(props.label) }
+    className={ classes }>
+      { props.label }
+    </button>
   );
 };
 
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,28 +2,60 @@ import React, { Component } from "react";
 import "./Calculator.css";
 
 import Button from "../components/Button";
+import Display from "../components/Display";
 
 export default class Calculator extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.resetCalculator = this.resetCalculator.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+  }
+
+  /**
+   * Adiciona um digito no display
+   */
+  addDigit(digit) {
+    console.log(`add ${ digit }`);
+  }
+
+  /**
+   * Reinicia a calculadora
+   */
+  resetCalculator() {
+    console.log(`limpar`);
+  }
+
+  /**
+   * Define a operação
+   */
+  setOperation(operation) {
+    console.log(`set ${ operation }`);
+  }
+
   render() {
     return (
       <div className="calculator">
-        <Button label="AC"></Button>
-        <Button label="/"></Button>
-        <Button label="7"></Button>
-        <Button label="8"></Button>
-        <Button label="9"></Button>
-        <Button label="*"></Button>
-        <Button label="4"></Button>
-        <Button label="5"></Button>
-        <Button label="6"></Button>
-        <Button label="-"></Button>
-        <Button label="1"></Button>
-        <Button label="2"></Button>
-        <Button label="3"></Button>
-        <Button label="+"></Button>
-        <Button label="0"></Button>
-        <Button label="."></Button>
-        <Button label="="></Button>
+        <Display value={ 0 }></Display>
+        <Button label="AC" click={ this.resetCalculator } triple ></Button>
+        <Button label="/" click={ this.setOperation } operation ></Button>
+        <Button label="7" click={ this.addDigit }></Button>
+        <Button label="8" click={ this.addDigit }></Button>
+        <Button label="9" click={ this.addDigit }></Button>
+        <Button label="*" click={ this.setOperation } operation ></Button>
+        <Button label="4" click={ this.addDigit }></Button>
+        <Button label="5" click={ this.addDigit }></Button>
+        <Button label="6" click={ this.addDigit }></Button>
+        <Button label="-" click={ this.setOperation } operation ></Button>
+        <Button label="1" click={ this.addDigit }></Button>
+        <Button label="2" click={ this.addDigit }></Button>
+        <Button label="3" click={ this.addDigit }></Button>
+        <Button label="+" click={ this.setOperation } operation ></Button>
+        <Button label="0" click={ this.addDigit } double ></Button>
+        <Button label="." click={ this.addDigit }></Button>
+        <Button label="=" click={ this.setOperation } operation ></Button>
       </div>
     )
   }
