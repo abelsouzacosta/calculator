280e2b00bd5b8d3f5d06be6fa91c7e4be9f96ee7
Abel Souza <abelsouzacosta@gmail.com>
2021-05-01 13:57:42 -0300

    feat(calculator): adding calculator functionality

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import "./Calculator.css";
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class Calculator extends Component {
+
+  state = { ...initialState }
 
   constructor(props) {
     super(props);
@@ -18,27 +28,63 @@ export default class Calculator extends Component {
    * Adiciona um digito no display
    */
   addDigit(digit) {
-    console.log(`add ${ digit }`);
+    // nao pode haver dois pontos dentro da calculadora
+    if (digit === '.' && this.state.displayValue.includes(digit)) {
+      return;
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+
+    const displayValue = currentValue + digit 
+
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (digit !== '.') {
+      const position = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[position] = newValue;
+      this.setState({ values });
+    }
   }
 
   /**
    * Reinicia a calculadora
    */
   resetCalculator() {
-    console.log(`limpar`);
+    this.setState({ ...initialState });
   }
 
   /**
    * Define a operação
    */
   setOperation(operation) {
-    console.log(`set ${ operation }`);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      values[0] = eval(`${ values[0] } ${ currentOperation } ${ values[1] }`);
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      });
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={ 0 }></Display>
+        <Display value={ this.state.displayValue }></Display>
         <Button label="AC" click={ this.resetCalculator } triple ></Button>
         <Button label="/" click={ this.setOperation } operation ></Button>
         <Button label="7" click={ this.addDigit }></Button>
